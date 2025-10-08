685230211151d149d9873e32314d0513b4968dab
shivanshuJags <shivanshusahu899@gmail.com>
2025-10-09 00:29:08 +0530

    fixes on user and auth controller

--- src/models/daily-service.model.js
+++ src/models/daily-service.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { VERIFICATION_STATUS } from "../utils/constants.js";
 
 const DailyServiceSchema = new mongoose.Schema(
   {
@@ -20,7 +21,18 @@ const DailyServiceSchema = new mongoose.Schema(
     description: { type: String, trim: true },
     images: [{ type: String, trim: true }],
     averageRating: { type: String, trim: true },
-    verificationStatus: { type: String, default: "pending" },
+    verificationStatus: {
+      status: {
+        type: String,
+        enum: Object.values(VERIFICATION_STATUS),
+        default: VERIFICATION_STATUS.PENDING,
+      },
+      rejectionReason: { type: String, default: null },
+    },
+    report: {
+      reason: [{ type: String, trim: true }],
+      totalReportCount: { type: Number, default: 0 },
+    },
     societyIds: {
       type: [
         {
@@ -39,15 +51,16 @@ const DailyServiceSchema = new mongoose.Schema(
     reviews: {
       type: [
         {
+          _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
           userId: {
             type: mongoose.Schema.Types.ObjectId,
             ref: "User",
             required: true,
           },
+          userName: { type: String, trim: true },
           rating: { type: Number, min: 1, max: 5, required: true },
           comment: { type: String, trim: true },
           createdAt: { type: Date, default: Date.now },
-          reply: { type: String, trim: true },
         },
       ],
       default: null,
